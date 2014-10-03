ca8d1ad1c2697f195aef81e987913bdc0051da61
Jacob Essex <jacob@jacobessex.com>
2014-10-03 18:18:31 +0100

    Updated sync.rb with options and made independent of cwd

--- sync.rb
+++ sync.rb
@@ -1,10 +1,11 @@
 #! /usr/bin/env ruby
 
 require 'fileutils'
+require 'optparse'
 
 def update_symlink(original_file, sym_target)
   original_file_path = File.expand_path original_file
-  sym_target_path    = File.expand_path sym_target.to_s
+  sym_target_path    = File.expand_path(File.join(File.dirname(__FILE__), sym_target.to_s))
 
   if File.exists? original_file_path
     puts "Removing #{original_file_path}"
@@ -20,20 +21,50 @@ def ensure_directory(dependency)
   Dir.mkdir dependency_path unless File.exists? dependency_path
 end
 
-update_symlink '~/.vimrc', 'vim/.vimrc'
-update_symlink '~/.vim', 'vim'
+use_git = false
+use_xmonad = false
+use_x = false
+use_ssh = false
 
-update_symlink '~/.xmonad', 'xmonad'
+use_vim = true
+use_zsh = true
 
-update_symlink '~/.oh-my-zsh', 'zsh/oh-my-zsh'
-update_symlink '~/.zshrc', 'zsh/.zshrc'
+OptionParser.new do |o|
+  o.on('--git') { use_git= true }
+  o.on('--xmonad') { use_xmonad = true  }
+  o.on('--x') { use_x = true }
+  o.on('--ssh') { use_ssh = true }
 
-update_symlink '~/.xmobarrc', 'xmobar/.xmobarrc'
+  o.on('--novim') { use_vim = false }
+  o.on('--nozsh') { use_zsh = false }
+  o.parse!
+end
 
-update_symlink '~/.gitconfig', 'git/.gitconfig'
+if !no_vim
+  update_symlink '~/.vimrc', 'vim/.vimrc'
+  update_symlink '~/.vim', 'vim'
+end
 
-update_symlink '~/.xresources', 'x/.xresources'
-update_symlink '~/.xrdb', 'x/xrdb'
+if !no_zsh
+  update_symlink '~/.oh-my-zsh', 'zsh/oh-my-zsh'
+  update_symlink '~/.zshrc', 'zsh/.zshrc'
+end
 
-ensure_directory '~/.ssh'
-update_symlink '~/.ssh/config', 'ssh/config'
+if use_xmonad
+  update_symlink '~/.xmonad', 'xmonad'
+  update_symlink '~/.xmobarrc', 'xmobar/.xmobarrc'
+end
+
+if use_git
+  update_symlink '~/.gitconfig', 'git/.gitconfig'
+end
+
+if use_x
+  update_symlink '~/.xresources', 'x/.xresources'
+  update_symlink '~/.xrdb', 'x/xrdb'
+end
+
+if use_ssh
+  ensure_directory '~/.ssh'
+  update_symlink '~/.ssh/config', 'ssh/config'
+end
